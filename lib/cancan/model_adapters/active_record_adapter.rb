1fb04ec8fbd7bc41d093117fb936d20da1642bb1
Matt Brictson <matt@mattbrictson.com>
2015-08-20 19:03:20 -0700

    Fix Rails 4.2 sanitize_sql deprecation warning

--- lib/cancan/model_adapters/active_record_adapter.rb
+++ lib/cancan/model_adapters/active_record_adapter.rb
@@ -149,8 +149,20 @@ module CanCan
         sanitize_sql(['?=?', true, true])
       end
 
+      # Rails 4.2 deprecates `sanitize_sql_hash_for_conditions`
       def sanitize_sql(conditions)
-        @model_class.send(:sanitize_sql, conditions)
+        if ActiveRecord::VERSION::MINOR >= 2 && Hash === conditions
+          table = Arel::Table.new(@model_class.send(:table_name))
+
+          conditions = ActiveRecord::PredicateBuilder.resolve_column_aliases @model_class, conditions
+          conditions = @model_class.send(:expand_hash_conditions_for_aggregates, conditions)
+
+          ActiveRecord::PredicateBuilder.build_from_hash(@model_class, conditions, table).map { |b|
+            @model_class.send(:connection).visitor.compile b
+          }.join(' AND ')
+        else
+          @model_class.send(:sanitize_sql, conditions)
+        end
       end
 
       # Takes two hashes and does a deep merge.
